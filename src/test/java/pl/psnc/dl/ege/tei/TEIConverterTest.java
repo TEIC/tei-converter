94d49738d896da7fecd385ae4ba8e5f8ea9e8b66
Anne Ferger <anne.ferger@uni-paderborn.de>
2025-04-15 16:25:22 +0200

    fix odd conversion and add unit test for it

--- src/test/java/pl/psnc/dl/ege/tei/TEIConverterTest.java
+++ src/test/java/pl/psnc/dl/ege/tei/TEIConverterTest.java
@@ -28,6 +28,8 @@ public class TEIConverterTest {
     public void convert() throws IOException, ConverterException {
         InputStream is = new FileInputStream("src/test/resources/test-input.xml.zip");
         OutputStream os = new FileOutputStream("src/test/resources/test-output.txt.zip");
+        InputStream is2 = new FileInputStream("src/test/resources/test-input.odd.zip");
+        OutputStream os2 = new FileOutputStream("src/test/resources/test-output.xml.zip");
         DataType inputType = new DataType("TEI","text/xml");
         DataType outputType = new DataType("txt","text/plain");
         ConversionActionArguments conversionActionArguments = new ConversionActionArguments(inputType, outputType, null);
@@ -39,9 +41,22 @@ public class TEIConverterTest {
         assertEquals("Text files differ",
                 new String(Files.readAllBytes(Paths.get("src/test/resources/expected-output.txt"))),
                 new String(Files.readAllBytes(Paths.get("src/test/resources/test-output.txt/document.txt"))));
+        inputType = new DataType("ODDC","text/xml");
+        outputType = new DataType("TEI","text/xml");
+        conversionActionArguments = new ConversionActionArguments(inputType, outputType, null);
+        converter.convert(is2, os2, conversionActionArguments, tempDir);
+        assertNotNull(new File("src/test/resources/test-output.xml.zip"));
+        InputStream isout2 = new FileInputStream("src/test/resources/test-output.xml.zip");
+        EGEConfigurationManager.getInstance().getStandardIOResolver().decompressStream(isout2, new File("src/test/resources/test-output.xml"));
+        assertEquals("Text files differ",
+                new String(Files.readAllBytes(Paths.get("src/test/resources/expected-output.xml"))),
+                new String(Files.readAllBytes(Paths.get("src/test/resources/test-output.xml/document.xml"))));
         is.close();
         os.close();
+        is2.close();
+        os2.close();
         isout.close();
+        isout2.close();
     }
 
     @org.junit.Test
